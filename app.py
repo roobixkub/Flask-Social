dd497e03ed99dbd26ffd019d9e2c9acb9e8285a6
Nate Pease <64239640+roobixkub@users.noreply.github.com>
2020-06-15 19:24:27 -0500

    Add style sheets and homepage post stream

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ Created on Sat Jun 13 21:48:02 2020
 from flask import (Flask, g, render_template, flash, redirect, url_for)
 from flask_bcrypt import check_password_hash
 from flask_login import (LoginManager, login_user, logout_user, 
-                         login_required)
+                         login_required, current_user)
 
 import forms
 import models
@@ -39,6 +39,7 @@ def before_request():
     """Connect to the database before each request."""
     g.db = models.DATABASE
     g.db.connect()
+    g.user = current_user
     
 
 @app.after_request
@@ -88,9 +89,38 @@ def logout():
     return redirect(url_for('index'))
 
 
+@app.route('/new_post', methods=('GET', 'POST'))
+@login_required
+def post():
+    form = forms.PostForm()
+    if form.validate_on_submit():
+        models.Post.create(user=g.user._get_current_object(), 
+                           content=form.content.data.strip())
+        flash("Message posted! Thanks!", "success")
+        return redirect(url_for('index'))
+    return render_template('post.html', form=form)
+
+
 @app.route('/')
 def index():
-    return 'Hey'
+    stream = models.Post.select().limit(100)
+    return render_template('stream.html', stream=stream)
+
+
+@app.route('/stream')
+@app.route('/stream/<username>')
+def stream(username=None):
+    template = 'stream.html'
+    if username and username != current_user.username:
+        user = models.User.select().where(models.User.username**username)
+        stream = user.posts.limit(100)
+    else:
+        stream = current_user.get_stream().limit(100)
+        user = current_user
+    if username:
+        template = 'user_stream.html'
+    return render_template(template, stream=stream, user=user)
+
 
 if __name__ == '__main__':
     models.initialize()
